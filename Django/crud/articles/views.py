374ac6ffaae083e4db9fbbef370c1b1398bb9920
Haneum Jung <zkdlwudhqm@naver.com>
2020-06-18 17:23:54 +0900

    Django 200618

--- Django/crud/articles/views.py
+++ Django/crud/articles/views.py
@@ -11,11 +11,20 @@ def index(request):
     }
     return render(request, 'articles/index.html', context)
 
-def new(request): # GET
-    context = {
+def new(request): # GET + POST
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
 
-    }
-    return render(request, 'articles/new.html', context)
+        # Database에 저장
+        # 1. Article 인스턴스 생성
+        article = Article(title=title, content=content)
+        # 2. 저장!
+        article.save()
+
+        return redirect('articles:detail', article.pk)
+    else:
+        return render(request, 'articles/new.html')
     
 def create(request): # POST
     title = request.POST.get('title')
@@ -27,7 +36,7 @@ def create(request): # POST
     # 2. 저장!
     article.save()
 
-    return redirect(f'/articles/detail/{article.pk}/')
+    return redirect('articles:detail', article.pk)
     # context = {
     #    'title': title,
     #    'content': content,
@@ -50,16 +59,36 @@ def delete(request, pk): # POST
     # 2. 삭제
     article.delete()
 
-    return redirect('/articles/index/')
+    return redirect('articles:index')
 
 def edit(request, pk): # GET
-    # Database 조회( + 저장)
     # 1. 조회
     article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        # 게시글 수정!
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        # Database 조회 + 수정 + 저장
+        # 1. 조회
+        article = Article.objects.get(pk=pk)
+        # 2. 수정
+        article.title = title
+        article.content = content
+        # 3. 저장
+        article.save()
+
+        return redirect('articles:detail', article.pk)
+    else:
+        # 게시글 수정 양식!
+
+    # Database 조회( + 저장)
+
+   
     
-    context = {
-        'article': article,
-    }    
+        context = {
+            'article': article,
+        }    
     return render(request, 'articles/edit.html', context)
 
 def update(request, pk): # POST
@@ -75,4 +104,4 @@ def update(request, pk): # POST
     # 3. 저장
     article.save()
 
-    return redirect(f'/articles/detail/{ article.pk }/')
+    return redirect('articles:detail', article.pk)
